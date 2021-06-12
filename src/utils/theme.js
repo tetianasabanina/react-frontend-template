77ff8c328df0083cc910d6d5658f351dcaa220d5
tatianasabanina <tatiana08st@gmail.com>
2021-06-12 20:14:28 +0300

    Create basic style theme

---
+++ src/utils/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#5CDB95'
+    },
+    secondary: {
+      main: '#EDF5E1'
+    }
+  }
+});
+
+export default theme;
